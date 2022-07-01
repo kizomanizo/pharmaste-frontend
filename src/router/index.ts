8ed51ff8bea0f58cca9948a11cc8bcc5545b2f06
Kizito M <kizomanizo@gmail.com>
2022-07-01 21:32:06 +0300

    Minor Updates

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import SalesView from '../views/SalesView.vue'
 import OrdersView from '../views/OrdersView.vue'
 import ProductsView from '../views/ProductsView.vue'
 import SettingsView from '../views/SettingsView.vue'
-
+    
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
