a64f0f31e531d262bd0ae4c5875d2add0d5de74b
Kizito M <kizomanizo@gmail.com>
2022-07-01 21:37:55 +0300

    Fishy update

--- src/stores/title.ts
+++ src/stores/title.ts
@@ -2,16 +2,20 @@ import { defineStore } from 'pinia'
 
 export const useTitle = defineStore({
     id: 'title',
+
     state: () => ({
         title: "Mwanzo",
         isVisible: true
     }),
+
     getters: {
         getTitle: (state) => state.title
     },
+
     actions: {
         changeTitle(heading: string) {
             this.title = heading
         }
     }
+    // here is a fish <*)))>{
 })
